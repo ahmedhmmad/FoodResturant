fdb7899f264395b786e967288f7be5dadc3068f7
ahmedhmmad <Ahmed.hammad@students.iugaza.edu.ps>
2022-05-07 02:05:20 +0300

    finish navbar relates #2

--- src/Components/Navs/Navs.jsx
+++ src/Components/Navs/Navs.jsx
@@ -1,22 +1,28 @@
 import React from "react";
 import { Container, Navbar, Nav } from "react-bootstrap";
+import "./Navs.css";
+import logo from "../../assets/logo.png";
 
 function Navs() {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar expand="lg">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">
+          <img src={logo} title="logo" alt="logo" />
+        </Navbar.Brand>
+
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
+          <Nav className="ms-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#link">About Us</Nav.Link>
             <Nav.Link href="#link">Explore Foods</Nav.Link>
             <Nav.Link href="#link">Reviews</Nav.Link>
             <Nav.Link href="#link">FAQ</Nav.Link>
-          </Nav>
-          <Nav>
-            <Nav.Link href="#">1800 500 500</Nav.Link>
+
+            <Nav.Link id="last-nav" href="#">
+              1800 500 500
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
